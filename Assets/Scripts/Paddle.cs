59dd3e6760120d7bec88111cedf4520cdde5c0b5
ortofone24 <ortofone24@gmail.com>
2019-07-07 07:01:07 +0200

    test

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float paddleInitialY;
+    private float defaultPaddleWidthInPixels = 200;
+    private SpriteRenderer sr;
+    private float defaultLeftClamp = 135;
+    private float defaultRightClamp = 410;
+
+    private void Start()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+        paddleInitialY = this.transform.position.y;
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        PaddleMovement();
+    }
+
+    private void PaddleMovement()
+    {
+        float paddleShift = (defaultPaddleWidthInPixels - ((defaultPaddleWidthInPixels / 2) * sr.size.x)) / 2;
+        float leftClamp = defaultLeftClamp - paddleShift;
+        float rightClamp = defaultRightClamp + paddleShift;
+        float mousePostionPixels = Mathf.Clamp(Input.mousePosition.x, leftClamp, rightClamp);
+        float mousePositionWorldX = mainCamera.ScreenToWorldPoint(new Vector3(mousePostionPixels, 0, 0)).x;
+        this.transform.position = new Vector3(mousePositionWorldX, paddleInitialY, 0);
+    }
+}
