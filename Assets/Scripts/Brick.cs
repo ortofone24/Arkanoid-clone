3a29d660b83405b26ba96e821eb88817b3bd5bab
ortofone24 <ortofone24@gmail.com>
2019-07-14 02:48:39 +0200

    levels from file

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -21,11 +21,10 @@ public class Brick : MonoBehaviour
 
         ApplyCollisionLogic(ball);
     }
-
-    private void Start()
+       
+    private void Awake()
     {
         this.sr = this.GetComponent<SpriteRenderer>();
-        this.sr.sprite = BricksManager.Instance.Sprites[this.Hitpoints - 1]; // will be deleted and set in Init.
     }
 
     private void ApplyCollisionLogic(Ball ball)
@@ -54,5 +53,13 @@ public class Brick : MonoBehaviour
         MainModule mm = effect.GetComponent<ParticleSystem>().main;
         mm.startColor = this.sr.color;
         Destroy(effect, DestroyEffect.main.startLifetime.constant);
+    }
+
+    public void Init(Transform containerTransform, Sprite sprite, Color color, int hitpoints)
+    {
+        this.transform.SetParent(containerTransform);
+        this.sr.sprite = sprite;
+        this.sr.color = color;
+        this.Hitpoints = hitpoints;
     }
 }
